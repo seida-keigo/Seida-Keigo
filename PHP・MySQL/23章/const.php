91343eb0db76948b2739f8ae109cdb80c277945b
keigo <k29gox@gmail.com>
2022-09-17 11:58:30 +0900

    first commit

---
+++ PHP・MySQL/23章/const.php
@@ -0,0 +1,8 @@
+<?php
+	define('TAX',1.05);
+	define('DB_HOST','mysql34.conoha.ne.jp');
+	define('DB_USER','bcdhm_work02');
+	define('DB_PASSWD','********');
+	define('DB_NAME','bcdhm_work02');
+	define('HTML_CHARACTER_SET','UTF-8');
+	define('DB_CHARACTER_SET','UTF8');
