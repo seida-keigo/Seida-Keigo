f6307e03d385561357c8a5a1d1a732503aa49f84
keigo <k29gox@gmail.com>
2022-12-04 21:39:31 +0900

    a

---
+++ JS整形.js
@@ -0,0 +1,30 @@
+for(let i=0;i<$.split(/\n/).length;++i){
+	let tabs=$.split(/\n/)[i].match(/^\t*/g)[0]
+	switch(($.split(/\n/)[i].match(/;|{|}/g)||0)[0]){
+		case";":
+		$=$.split(/\n/)
+		$[i]=$[i].replace(/;/,";\n"+tabs)
+		$=$.join("\n")
+		break
+		case"{":
+		$=$.split(/\n/)
+		$[i]=$[i].replace(/{/,"{\n\t"+tabs)
+		$=$.join("\n")
+		break
+		case"}":
+		if(!$.split(/\n/)[i].match(/^}/g))
+		if(!$.split(/\n/)[i].match(/\t}/g)){
+			$=$.split(/\n/)
+			$[i]=$[i].replace(/}/,"\n"+tabs.replace(/\t$/,"")+"}")
+			$=$.join("\n")
+		}
+	}
+}
+$
+
+document.querySelectorAll(".ql-editor")[0].innerText="1234"
+setTimeout(()=>document.querySelectorAll("[data-qa=texty_send_button]")[0].click(),0)
+
+log=console.log
+delete console
+log("something")
