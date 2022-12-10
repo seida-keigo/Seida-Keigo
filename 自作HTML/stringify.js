7ede1a8efa3da70a986fd3ff32dbc2960b1ccddd
keigo <k29gox@gmail.com>
2022-12-10 18:22:34 +0900

    a

--- 自作HTML/stringify.js
+++ 自作HTML/stringify.js
@@ -4,10 +4,10 @@ let stringify=item=>{
 	if(typeof item=="function")return item;
 	if(typeof item=="number")return item;
 	if(item===null)return"null";
-	//if(item.constructor.name=="Array")return"["+item.map(element=>stringify(element)).join(",")+"]";
-	//if(item.constructor.name=="Object")return"{"+Object.keys(item).map(key=>"\""+key+"\":"+stringify(item[key])).join(",")+"}";
-	if(typeof item=="object")return"Object.assign(new "+item.constructor.name+",{"+Object.keys(item).map(key=>"\""+key+"\":"+stringify(item[key])).join(",")+"})";
+	if(item.constructor.name=="Object")return"{"+Object.keys(item).map(key=>"\""+key+"\":"+stringify(item[key])).join(",")+"}";
+	if(item.constructor.name=="RegExp")return item;
+	if(typeof item=="object"||typeof item=="symbol")return"Object.assign(new "+item.constructor.name+",{"+[...Object.keys(item),...Object.getOwnPropertyNames(item.constructor.prototype)].map(key=>"\""+key+"\":"+stringify(item[key])).join(",")+"})";
 	if(typeof item=="string")return JSON.stringify(item);
-	if(typeof item=="symbol")return"Symbol("+(item.description!==undefined&&item.description!==""?"\""+item.description+"\"":"")+")";
+	//if(typeof item=="symbol")return"Symbol("+(item.description!==undefined&&item.description!==""?"\""+item.description+"\"":"")+")";
 	if(item===undefined)return;
 }
