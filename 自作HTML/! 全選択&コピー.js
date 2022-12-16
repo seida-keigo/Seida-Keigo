11a4417cc267a38e7a8ff18fe81c42b07c34f2ea
清田 圭吾 <k29gox@gmail.com>
2022-12-16 11:59:07 +0900

    a

--- 自作HTML/! 全選択&コピー.js
+++
@@ -1,8 +0,0 @@
-document.onclick=()=>{
-	let selection=getSelection()
-	let range=document.createRange()
-	range.selectNodeContents(document.body)
-	selection.removeAllRanges()
-	selection.addRange(range)
-	document.execCommand("copy")
-}
